f7ddd2cbd3e1d1e4a44ff5647de2b38d8eec9d33
TWSTYPH <ph_yang7655@163.com>
2023-03-03 15:40:49 +0800

    Linux 下完成 C++编译调试 通过设置tasks  和launch 文件

---
+++ 7/build/CMakeFiles/my_cmake_run.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_cmake_run.dir/main.cpp.o"
+  "CMakeFiles/my_cmake_run.dir/src/Gun.cpp.o"
+  "CMakeFiles/my_cmake_run.dir/src/Soldier.cpp.o"
+  "my_cmake_run.pdb"
+  "my_cmake_run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_cmake_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
